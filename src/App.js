7dbb6b2d0d432af8b22aa1db22360105061f4a44
sami-ul-haq <samiulhaq1899@gmail.com>
2020-10-07 16:50:59 +0500

    react-routing-practcie

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { Route, Switch, Link, BrowserRouter as Router } from 'react-router-dom';
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Home from './components/Home/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+
+            <div className="App">
+
+                <ul className="header">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+
+                <div className="content">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/about" component={About} />
+                        <Route path="/contact" component={Contact} />
+                    </Switch>
+                </div>
+            </div>
+
+        </Router>
+
+    )
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,23 @@
+// import React from "react";
+// import './Card.css'
+
+// const Card = (props) => {
+//     return (
+//         <>
+//             <div className="cards">
+//                 <div className="card">
+//                     <img src="https://picsum.photos/200/200" alt="myPic" />
+//                     <div className="card_info">
+//                         <span className="card_category">{props.cat}</span>
+//                         <h1 className="card_title">{props.title}</h1>
+//                         <a href="google.com">
+//                             <button>Watch Now</button>
+//                         </a>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     );
+// };
+
+// export default Card;
